11315b38696ec1b78231ab8e33ed19dc640cccab
Santi D <demagistris.santiago@gmail.com>
2021-04-09 12:52:35 -0300

    [Ejercicio Philosophers] Terminado

--- Clase4/Practica1/Ejemplos/philosophers.c
+++ Clase4/Practica1/Ejemplos/philosophers.c
@@ -6,6 +6,9 @@
 #define N_FILOSOFOS 5
 #define ESPERA 5000000
 
+pthread_mutex_t izquierda;
+
+
 pthread_mutex_t tenedor[N_FILOSOFOS];
 
 void pensar(int i)
@@ -20,26 +23,55 @@ void comer(int i)
   usleep(random() % ESPERA);
 }
 
-void tomar_tenedores(int i)
+void tomar_tenedores_d(int i)
 {
   pthread_mutex_lock(&tenedor[i]); /* Toma el tenedor a su derecha */
   pthread_mutex_lock(&tenedor[(i+1)%N_FILOSOFOS]); /* Toma el tenedor a su izquierda */
 }
 
-void dejar_tenedores(int i)
+void tomar_tenedores_i(int i)
+{
+  pthread_mutex_lock(&tenedor[(i+1)%N_FILOSOFOS]); /* Toma el tenedor a su izquierda */
+  pthread_mutex_lock(&tenedor[i]); /* Toma el tenedor a su derecha */
+}
+
+void dejar_tenedores_d(int i)
 {
   pthread_mutex_unlock(&tenedor[i]); /* Deja el tenedor de su derecha */
   pthread_mutex_unlock(&tenedor[(i+1)%N_FILOSOFOS]); /* Deja el tenedor de su izquierda */
 }
 
+void dejar_tenedores_i(int i)
+{
+  pthread_mutex_unlock(&tenedor[(i+1)%N_FILOSOFOS]); /* Deja el tenedor de su izquierda */
+  pthread_mutex_unlock(&tenedor[i]); /* Deja el tenedor de su derecha */
+}
+
 void *filosofo(void *arg)
 {
-  int i = (int)arg;
-  for (;;)
+  int i = *(int*)arg;
+  printf("Filosofo [%d]\n",i);
+
+  while(1)
   {
-    tomar_tenedores(i);
+
+    //pthread_mutex_lock(&izquierda);
+    //izquierda++;
+    if(i%2==0){
+      tomar_tenedores_d(i);
+      
+    }else{
+      tomar_tenedores_i(i);
+    }
+    
     comer(i);
-    dejar_tenedores(i);
+
+    if(i%2!=0){
+      dejar_tenedores_i(i);
+    }else{
+      dejar_tenedores_d(i);
+    }
+    
     pensar(i);
   }
 }
@@ -50,8 +82,14 @@ int main()
   pthread_t filo[N_FILOSOFOS];
   for (i=0;i<N_FILOSOFOS;i++)
     pthread_mutex_init(&tenedor[i], NULL);
-  for (i=0;i<N_FILOSOFOS;i++)
-    pthread_create(&filo[i], NULL, filosofo, (void *)i);
+  
+  //pthread_mutex_init(&fuiYo, NULL);
+  //pthread_mutex_init(&izquierda, NULL);
+  int args[5];
+  for (i=0;i<N_FILOSOFOS;i++){
+    args[i]=i;
+    pthread_create(&filo[i], NULL, filosofo, (void *)&args[i]);
+  }
   pthread_join(filo[0], NULL);
   return 0;
 }
