15562d8a40483ffa7a1cced3a0e645cd818c626f
Unknown <demagistris.santiago@gmail.com>
2021-06-10 11:29:15 -0300

    [Erlang] [Tp Final] Luchando con el algoritmo
    Seguimos en el sender, ya no se ni lo que hicimos

--- Trabajo Final/BroadcastAtomico/broadcastAtomico.erl
+++ Trabajo Final/BroadcastAtomico/broadcastAtomico.erl
@@ -8,15 +8,15 @@
 %%Libreria de acceso
 -export([aBroadcast/1]).
 
--export([aDeliver/0,aSequencer/3,aSender/0]).
+-export([aDeliver/0,aSequencer/3,aSender/2]).
 
 -define(Nodos,4).
 
 start() ->
 
-    register(sequencer, spawn(?MODULE, aSecuencer,[noMensaje,0,0,dict:new()])),
+    register(sequencer, spawn(?MODULE, aSecuencer,[dict:new(),0,0])),
     register(deliver,   spawn(?MODULE, aDeliver  ,[])),
-    register(sender,    spawn(?MODULE, aSender   ,[])),
+    register(sender,    spawn(?MODULE, aSender   ,[0,dict:new()])),
     ok.
 
 stop() ->
@@ -24,34 +24,9 @@ stop() ->
 
 
 proposalColector(Lista)
-receive
-->ok.
+    receive
 
-aSender() ->
-
-    receive 
-        
-        fin -> exit(normal);
-
-        Mensaje -> 
-
-        sequencer ! getPaquetInfo,
-
-        receive
-
-            {paquetInfo,OrdenMaximo,CantMensajesEnviados} ->
-                    PaqueteSO = #paqueteSinOrden{msg = Mensaje
-                                               ,claveMensaje = {node(),CantMensajesEnviados}
-                                               ,ultimoOrdenAcordado = OrdenMaximo},
-
-                    lists:foreach(fun(X) -> {sequencer , X } ! {askingForOrder,PaqueteSO} end,nodes()),
-                    FinalPropuesta=lists:max(proposalColector([]))
-        end
-
-
-
-
-
+    ->ok
     end.
 
 aBroadcast(Mensaje) ->
@@ -60,7 +35,102 @@ aBroadcast(Mensaje) ->
     ok.
 
 
+contarElementos([Hd])->
+    1.
+contarElementos([Hd|Tl])->
+    1 + contarElementos([Tl]).
+
+
+aSender(CantMensajesEnviados,DicMsgToSend) ->
+
+    receive 
+        
+        fin -> exit(normal);
+
+        Mensaje -> 
+
+        sequencer ! {getPaquetInfo, Mensaje, CantMensajesEnviados + 1},
+
+        receive
+
+            {paquetInfo,OrdenMaximoAcordado} ->
+                    PaqueteSO = #paqueteSinOrden{msg = Mensaje
+                                               ,claveMensaje = {node(),CantMensajesEnviados}
+                                               ,ultimoOrdenAcordado = OrdenMaximoAcordado},
+
+                    CantPropuestasARecebir = contarElementos(nodes()),
+
+                    NewDic = dict:append(PaqueteSO#paqueteSinOrden.claveMensaje
+                            , {CantPropuestasARecebir,[]}
+                            , DicMsgToSend),
+
+                lists:foreach(fun(X) -> {sequencer , X } ! {askingForOrder,PaqueteSO} end,nodes())
+        end,
+
+        aSender(CantMensajesEnviados,NewDic);
+
+        
+        {propuestaOrden, PropuestaOrden, IdentificadorMensaje} ->
+
+            case dict:find(IdentificadorMensaje, DicMsgToSend)
+                {ok, Value} ->
+                    {CantPropuestasARecebir,ListaPropuestas} = Value,
+                    PropToReceive = CantPropuestasARecebir - 1,
+                    ListaPrima = ListaPropuestas ++ [PropuestaOrden],
+
+                    if 
+                        (PropToReceive == 0) ->
+                                            PropuestaFinal = lists:max(ListaPrima),
+                                            NewDic = dict:erase(IdentificadorMensaje, DicMsgToSend)
+                                            lists:foreach(fun(X) -> {sequencer , X } ! {propuestaFinal,PropuestaFinal} end,nodes()),
+                                            sequencer ! {propuestaFinal,PropuestaFinal},
+                                            aSender(CantMensajesEnviados,NewDic);   
+
+                                            
+                        true->
+                            NewDic = dict:store(IdentificadorMensaje
+                                    , {PropToReceive,ListaPrima}
+                                    , DicMsgToSend),
+                            aSender(CantMensajesEnviados,NewDic)        
+                    end;
+                    
+                error ->
+                    error. %% se muere, como debe ser... por inepto 
+            end
+
+
+    end.
+
+
+aSequencer(DicMensajes,OrdenMaximoAcordado,OrdenMaximoPropuesto)-> 
+
+    receive
+        {askingForOrder,Paquete} when is_record(Paquete,paqueteSinOrden) -> 
+            %% [clave = {Nodo,CantMensajesEnviados}] => {Mensaje , propuestaValor}   , valorDefinitivo => Mensaje
+            PropuestaOrden = lists:max([OrdenMaximoPropuesto,Paquete#paqueteSinOrden.ultimoOrdenAcordado]) + 1,
+            {Nodo,_} = Paquete#paqueteSinOrden.claveMensaje,            
+            {sender, Nodo} ! {propuestaOrden, PropuestaOrden,Paquete#paqueteSinOrden.claveMensaje},
+
+            dict:append(PaqueteSO#paqueteSinOrden.claveMensaje
+                            , {PaqueteSO#paqueteSinOrden.msg, PropuestaOrden}
+                            , DicMensajes),
+
+            aSequencer(DicMensajes, OrdenMaximoAcordado, PropuestaOrden);
+
+        {getPaquetInfo, Mensaje, CantMensajesEnviados} -> sender ! {paquetInfo,OrdenMaximoAcordado},
+                        PaqueteSO = #paqueteSinOrden{msg = Mensaje
+                                               ,claveMensaje = {node(),CantMensajesEnviados}
+                                               ,ultimoOrdenAcordado = OrdenMaximoPropuesto},
+
+                        dict:append(PaqueteSO#paqueteSinOrden.claveMensaje
+                            , {PaqueteSO#paqueteSinOrden.msg,PaqueteSO#paqueteSinOrden.ultimoOrdenAcordado}
+                            , DicMensajes),
+
+                        aSequencer(DicMensajes,OrdenMaximoAcordado,OrdenMaximoPropuesto + 1)
+    end.
+
 aDeliver() ->
+
     receive
         fin -> exit(normal);
         M ->
@@ -68,14 +138,5 @@ aDeliver() ->
             aDeliver()
     end.
 
-aSequencer(CantMensajesEnviados,OrdenMaximo,DicMensajesSeq)-> 
 
-    receive
-
-        {askingForOrder,Paquete} when is_record(Paquete,paqueteSinOrden) -> 
-
-            PropuestaOrden = lists:max([OrdenMaximo,Paquete#paqueteSinOrden.ultimoOrdenAcordado]) + 1,
-            sender ! {propuestaOrden, PropuestaOrden};
-
-        getPaquetInfo -> sender ! {paquetInfo,OrdenMaximo,CantMensajesEnviados}
-    end.
+%%Sharing Terminal: Could not start terminal process C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe: The directory name is invalid.
