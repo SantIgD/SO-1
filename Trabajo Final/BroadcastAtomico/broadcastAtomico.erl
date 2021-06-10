37b8902363c0d615a9265daa526f63f523bcfefb
Unknown <demagistris.santiago@gmail.com>
2021-06-10 12:14:21 -0300

    [Erlang] [Tp Final] Al parecer terminamos el algoritmo.
    Chequear envio a deliver con asequencer

--- Trabajo Final/BroadcastAtomico/broadcastAtomico.erl
+++ Trabajo Final/BroadcastAtomico/broadcastAtomico.erl
@@ -8,13 +8,13 @@
 %%Libreria de acceso
 -export([aBroadcast/1]).
 
--export([aDeliver/0,aSequencer/3,aSender/2]).
+-export([aDeliver/0,aSequencer/5,aSender/2]).
 
 -define(Nodos,4).
 
 start() ->
 
-    register(sequencer, spawn(?MODULE, aSecuencer,[dict:new(),0,0])),
+    register(sequencer, spawn(?MODULE, aSecuencer,[dict:new(),0,0,1,0])),
     register(deliver,   spawn(?MODULE, aDeliver  ,[])),
     register(sender,    spawn(?MODULE, aSender   ,[0,dict:new()])),
     ok.
@@ -80,13 +80,12 @@ aSender(CantMensajesEnviados,DicMsgToSend) ->
 
                     if 
                         (PropToReceive == 0) ->
-                                            PropuestaFinal = lists:max(ListaPrima),
+                                            PropuestaAcordada = lists:max(ListaPrima),
                                             NewDic = dict:erase(IdentificadorMensaje, DicMsgToSend)
-                                            lists:foreach(fun(X) -> {sequencer , X } ! {propuestaFinal,PropuestaFinal} end,nodes()),
-                                            sequencer ! {propuestaFinal,PropuestaFinal},
+                                            lists:foreach(fun(X) -> {sequencer , X } ! {PropuestaAcordada,IdentificadorMensaje,PropuestaAcordada} end,nodes()),
+                                            sequencer ! {propuestaAcordada,IdentificadorMensaje,PropuestaAcordada},
                                             aSender(CantMensajesEnviados,NewDic);   
-
-                                            
+                
                         true->
                             NewDic = dict:store(IdentificadorMensaje
                                     , {PropToReceive,ListaPrima}
@@ -95,17 +94,18 @@ aSender(CantMensajesEnviados,DicMsgToSend) ->
                     end;
                     
                 error ->
-                    error. %% se muere, como debe ser... por inepto 
+                    error %% se muere, como debe ser... por inepto 
             end
 
 
     end.
 
 
-aSequencer(DicMensajes,OrdenMaximoAcordado,OrdenMaximoPropuesto)-> 
+aSequencer(DicMensajes,OrdenMaximoAcordado,OrdenMaximoPropuesto, OrdenActual, TO)-> 
 
     receive
         {askingForOrder,Paquete} when is_record(Paquete,paqueteSinOrden) -> 
+            
             %% [clave = {Nodo,CantMensajesEnviados}] => {Mensaje , propuestaValor}   , valorDefinitivo => Mensaje
             PropuestaOrden = lists:max([OrdenMaximoPropuesto,Paquete#paqueteSinOrden.ultimoOrdenAcordado]) + 1,
             {Nodo,_} = Paquete#paqueteSinOrden.claveMensaje,            
@@ -116,6 +116,28 @@ aSequencer(DicMensajes,OrdenMaximoAcordado,OrdenMaximoPropuesto)->
                             , DicMensajes),
 
             aSequencer(DicMensajes, OrdenMaximoAcordado, PropuestaOrden);
+
+
+        {propuestaAcordada,IdentificadorMensaje,PropuestaAcordada} ->
+
+                NewOrdenMaxAcor = lists:max([PropuestaAcordada,OrdenMaximoAcordado]),
+                case dict:find(IdentificadorMensaje, DicMensajes) of
+
+                {ok, Msg} ->
+
+                    {Mensaje,_OrdenPropuesto} = Msg,
+                    AuxDic = dict:erase(IdentificadorMensaje, DicMsgToSend)   
+
+                    NewDic = dict:store(PropuestaAcordada
+                                    , Msg
+                                    , AuxDic),
+                    
+                    aSequencer(NewDic,NewOrdenMaxAcor,OrdenMaximoPropuesto);
+                   
+                error ->
+                    error
+                end,
+                
 
         {getPaquetInfo, Mensaje, CantMensajesEnviados} -> sender ! {paquetInfo,OrdenMaximoAcordado},
                         PaqueteSO = #paqueteSinOrden{msg = Mensaje
